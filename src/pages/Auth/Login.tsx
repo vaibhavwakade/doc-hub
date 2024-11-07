c4bce1bec39ca935260e94bfe182dfb9a7bfb45c
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-11-07 15:46:11 +0530

    feat : add login register component pages

---
+++ src/pages/Auth/Login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/features/Auth/Login/LoginForm";
+
+function Login() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <LoginForm />
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Auth/Register.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/features/Auth/Register/RegisterForm";
+
+function Register() {
+  return (
+    <div>
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default Register;
